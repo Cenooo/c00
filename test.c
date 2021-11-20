da6764f1e640f2c42674a4797fc5a7469a330104
Cenooo <94233626+Cenooo@users.noreply.github.com>
2021-11-20 13:50:35 +0100

    Add files via upload

---
+++ test.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpolat <cpolat@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/20 13:36:10 by cpolat            #+#    #+#             */
+/*   Updated: 2021/11/20 13:49:47 by cpolat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_print(int a, int b, int c)
+{
+	if (!(a == 7 && b == 8 && c == 9))
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = 0;
+	while (a <= 7)
+	{
+		b = a + 1;
+		while (b <= 8)
+		{
+			c = b + 1;
+			while (c <= 9)
+			{
+				ft_putchar('0' + a);
+				ft_putchar('0' + b);
+				ft_putchar('0' + c);
+				ft_print(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
