6de991c10bd551579c37a41b1d7e0370a859b906
Cenooo <94233626+Cenooo@users.noreply.github.com>
2021-11-20 13:25:48 +0100

    Update ft_print_reverse_alphabet.c

--- c00/ex02/ft_print_reverse_alphabet.c
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -16,7 +16,7 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_print_alphabet(void)
+void	ft_print_reverse_alphabet(void)
 {
 	char	a;
 
@@ -26,5 +26,4 @@ void	ft_print_alphabet(void)
 		ft_putchar(a);
 	a--;
 	}
-	ft_putchar('\n');
 }
